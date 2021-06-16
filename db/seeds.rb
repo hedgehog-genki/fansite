18722c833ba7f59d5f1640ad5c5dd220b8787514
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-16 22:55:02 +0900

    CSVファイルの削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,57 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "csv"
-CSV.foreach("db/archive.csv", headers: true) do |row|
-  Archive.create(
-    id: row["id"],
-    archiveid: row["archiveid"],
-    archivetitle: row["archivetitle"],
-    archiveviews: row["archiveviews"],
-    archivepostdate: row["archivepostdate"],
-    archiveurl: row["archiveurl"],
-    archiveimg: row["archiveimg"],
-    created_at: row["created_at"],
-    updated_at: row["updated_at"],
-  )
-end
+#require "csv"
+#CSV.foreach("db/archive.csv", headers: true) do |row|
+#  Archive.create(
+#    id: row["id"],
+#    archiveid: row["archiveid"],
+#    archivetitle: row["archivetitle"],
+#    archiveviews: row["archiveviews"],
+#    archivepostdate: row["archivepostdate"],
+#    archiveurl: row["archiveurl"],
+#    archiveimg: row["archiveimg"],
+#    created_at: row["created_at"],
+#    updated_at: row["updated_at"],
+#  )
+#end
 
-CSV.foreach("db/clip.csv",headers: true) do |row|
-  Clip.create(
-    id: row["id"],
-    clipid: row["clipid"],
-    cliptitle: row["cliptitle"],
-    clipurl: row["clipurl"],
-    clipview: row["clipview"],
-    clipimg: row["clipimg"],
-    clippostdate: row["clippostdate"],
-    created_at: row["created_at"],
-    updated_at: row["updated_at"],
-  )
-end
+#CSV.foreach("db/clip.csv",headers: true) do |row|
+#  Clip.create(
+#    id: row["id"],
+#    clipid: row["clipid"],
+#    cliptitle: row["cliptitle"],
+#    clipurl: row["clipurl"],
+#    clipview: row["clipview"],
+#    clipimg: row["clipimg"],
+#    clippostdate: row["clippostdate"],
+#    created_at: row["created_at"],
+#    updated_at: row["updated_at"],
+#  )
+#end
 
-CSV.foreach("db/music.csv",headers: true) do |row|
-  Music.create(
-    id: row["id"],
-    musicid: row["musicid"],
-    musictitle: row["musictitle"], 
-    musicauthor: row["musicauthor"],
-    musicurl: row["musicurl"],
-    musicimg: row["musicimg"],
-    created_at: row["created_at"],
-    updated_at: row["updated_at"],
-  )
-end
+#CSV.foreach("db/music.csv",headers: true) do |row|
+#  Music.create(
+#    id: row["id"],
+#    musicid: row["musicid"],
+#    musictitle: row["musictitle"], 
+#    musicauthor: row["musicauthor"],
+#    musicurl: row["musicurl"],
+#    musicimg: row["musicimg"],
+#    created_at: row["created_at"],
+#    updated_at: row["updated_at"],
+#  )
+#end
 
-CSV.foreach("db/fanart.csv",headers: true) do |row|
-  Fanart.create(
-    id: row["id"],
-    fanartid: row["fanartid"],
-    fanarturl: row["fanarturl"],
-    fanartuser: row["fanartuser"],
-    created_at: row["created_at"],
-    updated_at: row["updated_at"],
-  )
-end
+#CSV.foreach("db/fanart.csv",headers: true) do |row|
+#  Fanart.create(
+#    id: row["id"],
+#    fanartid: row["fanartid"],
+#    fanarturl: row["fanarturl"],
+#    fanartuser: row["fanartuser"],
+#    created_at: row["created_at"],
+#    updated_at: row["updated_at"],
+#  )
+#end
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
